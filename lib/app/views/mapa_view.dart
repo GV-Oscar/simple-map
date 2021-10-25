a0b310108387e0a7b1746c464a09cd3481329361
Oscar Giraldo <oscargiraldo000@gmail.com>
2021-10-25 18:59:57 -0500

    Logica de bloc de mapa y ubicacion

--- lib/app/views/mapa_view.dart
+++ lib/app/views/mapa_view.dart
@@ -1,14 +1,79 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:simple_map/app/bloc/map/map_bloc.dart';
+import 'package:simple_map/app/bloc/my_location/my_location_bloc.dart';
+import 'package:simple_map/app/widgets/widgets.dart';
 
+class MapaView extends StatefulWidget {
+  @override
+  State<MapaView> createState() => _MapaViewState();
+}
 
-class MapaView extends StatelessWidget {
+class _MapaViewState extends State<MapaView> {
+  late MapBloc _mapBloc;
+  late MyLocationBloc _locationBloc;
+
+  @override
+  void initState() {
+    _locationBloc = BlocProvider.of<MyLocationBloc>(context);
+    _mapBloc = BlocProvider.of<MapBloc>(context);
+
+    _locationBloc.startTrackingUserLocation();
+
+    // context.read<MyLocationBloc>().startTrackingUserLocation();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _locationBloc.stopTrackingUserLocation();
+
+    // BlocProvider.of<MyLocationBloc>(context).stopTrackingUserLocation();
+    // context.read<MyLocationBloc>().stopTrackingUserLocation();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('MapaView'),
-     ),
-   );
+      body: BlocBuilder<MyLocationBloc, MyLocationState>(
+        builder: (context, state) {
+          return Center(
+            child: createMap(state),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [BtnMyLocation(onPressed: onClickBtnMyLocation)],
+      ),
+    );
+  }
+
+  Widget createMap(MyLocationState state) {
+    if (!state.existsLocation) return Text('Ubicando...');
+
+    final cameraPosition = CameraPosition(
+      target: state.lastLocation!,
+      zoom: 15.0,
+    );
+
+    Completer<GoogleMapController> _controller = Completer();
+
+    return GoogleMap(
+      initialCameraPosition: cameraPosition,
+      onMapCreated: _mapBloc.initializeMap,
+      compassEnabled: true,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      zoomControlsEnabled: false,
+    );
+  }
+
+  onClickBtnMyLocation() {
+    _mapBloc.moveCamera(_locationBloc.lastLocation);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_map/app/bloc/map/map_bloc.dart';
+import 'package:simple_map/app/bloc/my_location/my_location_bloc.dart';
 import 'package:simple_map/app/views/acceso_gps_view.dart';
 import 'package:simple_map/app/views/loading_view.dart';
 import 'package:simple_map/app/views/mapa_view.dart';
@@ -8,15 +11,23 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Simple Map',
-      home: LoadingView(),
-      routes: {
-        'mapa': (_) => MapaView(),
-        'loading': (_) => LoadingView(),
-        'acceso_gep': (_) => AccesoGpsView()
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => MyLocationBloc()),
+        BlocProvider(create: (_) => MapBloc())
+
+        ///
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Simple Map',
+        home: LoadingView(),
+        routes: {
+          'mapa': (_) => MapaView(),
+          'loading': (_) => LoadingView(),
+          'acceso_gep': (_) => AccesoGpsView()
+        },
+      ),
     );
   }
 }
